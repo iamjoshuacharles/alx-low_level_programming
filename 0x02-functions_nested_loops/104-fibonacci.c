a30d89f0243464218b330811572ee05f63be3ccb
iamjoshuacharles <joshuacharles.n@gmail.com>
2022-09-26 11:26:44 -0700

    updated 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 /**
 * main - main block
 * Description: Find and print the first 98 fibonacci numbers starting with 1 and 2.
-* Numbers should be coma and space separated.
+* Numbers seperated by comma and space.
 * Return: 0
 */
 int main(void)
