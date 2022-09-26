02eb5f2bbc7d0a7f02928b1f46722433a9196e2c
iamjoshuacharles <joshuacharles.n@gmail.com>
2022-09-26 11:22:36 -0700

    a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
-* main - Prints the first 98 Fibonacci numbers, starting with
-*        1 and 2, separated by a comma followed by a space.
-*
-* Return: Always 0.
+* main - main block
+* Description: Find and print the first 98 fibonacci numbers starting with 1 and 2.
+* Numbers should be coma and space separated.
+* Return: 0
 */
 int main(void)
 {
-	int count = 2;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	long double fib1 = 1, fib2 = 2, sum;
-
-	printf("1, 2, ");
-
-	while (count < 98)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		sum = fib1 + fib2;
-		printf("%.0Lf", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-
-		if (count == 97)
-			printf("\n");
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
 		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
 			printf(", ");
-		count++;
 	}
-
+	printf("\n");
 	return (0);
 }
