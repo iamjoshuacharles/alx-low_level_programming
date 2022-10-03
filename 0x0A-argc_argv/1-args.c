9e83c33dfb157dda495bc768d7952e37cdb862a2
iamjoshuacharles <joshuacharles.n@gmail.com>
2022-10-03 06:58:28 -0700

    hot fix on number of arguments printed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,9 @@
 * Return: 0
 */
 
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	int i;
+	printf("%d\n", argc - 1);
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%d\n", argc - 1);
-	}
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,6 @@
 
 int _putchar(char c);
 int main(int argc, char *argv[]);
-int main(int argc, __attribute__((unused)) char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
 #endif /* MAIN_H */
 
