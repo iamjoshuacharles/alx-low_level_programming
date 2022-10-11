790bb8f3e241663723441171802a1072aa25fe6e
iamjoshuacharles <joshuacharles.n@gmail.com>
2022-10-10 17:27:04 -0700

    a function that prints a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef DOG_H
+ifndef DOG_H
 #define DOG_H
 
 /**
@@ -19,4 +19,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* DOG_H */
+#endif /* DOG_H */ 
